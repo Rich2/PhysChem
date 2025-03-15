ec13ae2269f57bac45b110e8387f6599fbc5c071
Rich2 <rr1@tutanota.com>
2025-03-15 14:53:04 +0000

    Added revolver plugin.

--- PartFx/src/FxApp.scala
+++ PartFx/src/FxApp.scala
@@ -1,12 +1,25 @@
  /* Copyright 2025 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ppart
-import ostrat.*
+import ostrat.*, javafx.*, stage.*, scene.*, canvas.*
 
 object FxApp
 {
-  def main(args: Array[String]): Unit =
-  {
-    deb("Starting FxApp")
-    println(Water.atoms)
+  def main(args: Array[String]): Unit = javafx.application.Application.launch(classOf[AppStart], args*)
+}
+
+class AppStart extends application.Application
+{
+  override def start(primaryStage: Stage): Unit =
+  { val bounds = stage.Screen.getPrimary.getVisualBounds
+    val canvWidth: Double = bounds.getWidth - 8
+    val canvHeight: Double = bounds.getHeight
+    val canvasCanvas: Canvas = new Canvas(canvWidth, canvHeight)
+    val root = new Group()
+    root.getChildren.add(canvasCanvas)
+    primaryStage.setX(0)//Sets default x value
+    val jScene = new Scene(root, canvWidth, canvHeight)
+    primaryStage.setTitle("Particles")
+    primaryStage.setScene(jScene)
+    primaryStage.show
   }
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # PhysChem
-Just thought I'd create some classes and objects for particles and elements
+Just thought I'd create some classes and objects for particles and elements. Protons, neutrons, electons, atoms amd molecules.
