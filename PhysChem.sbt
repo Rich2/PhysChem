d8876174d584adde24b9df0d4b43651e4efd7af2
Rich2 <rr1@tutanota.com>
2025-03-15 12:44:08 +0000

    Updated to Openstrat 0.3.6.

--- PhysChem.sbt
+++ PhysChem.sbt
@@ -2,8 +2,10 @@
 name := "PhysChem"
 version := "0.0.1"
 scalaVersion := "3.6.4"
-
-lazy val root = (project in file(".")).dependsOn(util, geom)
-lazy val util = ProjectRef(uri("https://github.com/Rich2/openstrat.git"), "Util")
-lazy val geom = ProjectRef(uri("https://github.com/Rich2/openstrat.git"), "Geom")
+lazy val root = (project in file("."))//.dependsOn(util, geom)
 Compile/scalaSource := baseDirectory.value / "src"
+//lazy val util = ProjectRef(uri("https://github.com/Rich2/openstrat.git"), "Util")
+//lazy val geom = ProjectRef(uri("https://github.com/Rich2/openstrat.git"), "Geom")
+resolvers += "RichStrat" at "https://richstrat.com/repository"
+libraryDependencies += "com.richstrat" % "rutil" % "0.3.6" withSources() withJavadoc()
+libraryDependencies += "com.richstrat" % "geom" % "0.3.6" withSources() withJavadoc()
--- src/Element.scala
+++ src/Element.scala
@@ -5,7 +5,7 @@ import ostrat.*, geom.*, Colour.*
 abstract class Element(val name: String, val abrev: String)
 { val atomicNum: Int
   def cpkColour: Colour
-  def radius: Angstroms = Angstroms(0.55)
+  def radius: Angstroms
   override def toString: String = name + "Atom"
 
   class Isotope(val numNeutrons: Int)
@@ -21,7 +21,7 @@ class Atom(val element: Element)
 object Hydrogen extends Element("Hydrogen", "H")
 { override val atomicNum: Int = 1
   override val cpkColour: Colour = White
-  override def radius: Angstroms = Angstroms(0.55)
+  override def radius: Angstroms = 0.55.angstroms
   object Protium extends Isotope(0)
   object Deuterium extends Isotope(1)
   object Tritium extends Isotope(2)
@@ -32,6 +32,7 @@ class HAtom extends Atom(Hydrogen)
 object Carbon extends Element("Carbon", "C")
 { override val atomicNum: Int = 6
   override val cpkColour: Colour = Black
+  override def radius: Angstroms = 0.70.angstroms
   object Carbon12 extends Isotope(6)
   object Carbon13 extends Isotope(7)
   object Carbon14 extends Isotope(8)
@@ -42,6 +43,7 @@ class CAtom extends Atom(Carbon)
 object Oxygen extends Element("Oxygen", "O")
 { override val atomicNum: Int = 8
   override val cpkColour: Colour = Red
+  override def radius: Angstroms = 0.73.angstroms
   object Oxygen16 extends Isotope(8)
 }
 
