f6e883d8ca14b89d9a16adf896c3377577d1f2a1
Rich2 <rr1@tutanota.com>
2025-03-16 14:35:29 +0000

    Using PtPm2Arr.

--- Particle/src/Molecule.scala
+++ Particle/src/Molecule.scala
@@ -1,11 +1,12 @@
 /* Copyright 2025 Richard Oliver. Licensed under Apache Licence version 2.0. */
 package ppart
-import ostrat.*
+import ostrat.*, geom.*
 
 /** A Chemical compound. */
 trait Molecule
 {
   val atoms: RArr[Atom]
+  def atomPosn: PtPm2Arr = ???
   val bonds: RPairArr[Atom, Atom]
 }
 
@@ -14,6 +15,8 @@ object Dihydrogen extends Molecule
   val aH1 = HAtom()
   val aH2 = HAtom()
   override val atoms: RArr[Atom] = RArr(aH1, aH2)
+
+  override def atomPosn = PtPm2Arr.fromDbls(-37,0 ,37,0)
   override val bonds: RPairArr[Atom, Atom] = RPairArr((aH1, aH2))
 }
 
